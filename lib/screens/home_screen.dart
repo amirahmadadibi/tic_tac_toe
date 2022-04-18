3d5dbc09a097b6be8a0ac526f1db5bf55b0a226e
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-04-19 00:18:00 +0430

    week7 E03-using gridbuilder to create grid of game

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,67 +6,85 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.grey[900],
-          title: Text('TicTacToe'),
-        ),
+      appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.grey[900],
-        body: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      'Player O',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      '0',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                      ),
-                    ),
-                  )
-                ],
-              ),
+        title: Text('TicTacToe'),
+      ),
+      backgroundColor: Colors.grey[900],
+      body: GridView.builder(
+        itemCount: 9,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
             ),
-            Expanded(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      'Player X',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                      ),
-                    ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget getScoreBorad() {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player O',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(
-                      '1',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
-            ),
-          ],
-        ));
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  '0',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player X',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  '1',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
